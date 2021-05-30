19b08f9a2e1c040e0609d72cc244db9ce844f1be
kim <torres.kim.dave@gmail.com>
2021-05-30 13:51:10 +0800

    cookies and passports

--- app.js
+++ app.js
@@ -12,3 +12,10 @@ const app = express();
 app.use(express.json()); // it allows access the data sent from frontend using req.body.
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, `client`))); // saying that our file html etc., will be served.
+
+
+/* Cookies and Passports */
+app.use(cookie_session({
+ cookie_expi: 24 * 60 * 60 * 1000, // it is the total expiration time the cookies will be alive for 1 day.
+ keys: [`1d21d6d5bffbb044b90d5ca56abf3446ff9a50e9`] // the string you can type whatever you want instead. 
+}));
