6badce08d6c1bf7549dfca857d9fbf2091a052cd
kim <torres.kim.dave@gmail.com>
2021-05-30 14:22:21 +0800

    config, routes, error handlers, port server

--- app.js
+++ app.js
@@ -19,3 +19,36 @@ app.use(cookie_session({
  cookie_expi: 24 * 60 * 60 * 1000, // it is the total expiration time the cookies will be alive for 1 day.
  keys: [`1d21d6d5bffbb044b90d5ca56abf3446ff9a50e9`] // the string you can type whatever you want instead. 
 }));
+
+
+/* Configuration */
+require(`./config/mongodbConfig`); // the mongodbConfig file that our database configuration.
+require(`./config/passportConfig`); // the passportConfig file that our signup-login configuration.
+
+
+/* Routes */
+app.use(require(`./routes/authRoute`)); // signup-login routes.
+// if there is any route that does not match the above routes, sends the index.html file as default page.
+app.get(`*`, (req, res, next) => { // request, responds.
+ try {
+  res.sendFile(path.join(__dirname, `client/index.html`));
+ } catch (err) {
+  next(err, req, res)
+ }
+});
+
+
+/* Error Handlers */
+app.use((err, req, res, next) => {
+ console.log(err.message);
+
+ console.log(err);
+ res.json({ msg: `Server Error`, error: err.message })
+});
+
+
+/* PORT */
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+ console.log(`Server is running on port ${PORT}`);
+});
