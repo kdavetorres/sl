4dc1b056455d8579cac2083841f488d45ae5ac10
kim <torres.kim.dave@gmail.com>
2021-05-30 13:02:15 +0800

    firing app express / served the files

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ const path = require(`path`); // it is built-in node module so no need to instal
 
 /* Signup-Login */
 const passport = require(`passport`); // this is the main package that will help create the signup-login system.
-const cookie_session = require(`cookie-session`); // this for using cookies to track users login session
+const cookie_session = require(`cookie-session`); // this for using cookies to track users login session.
+
+
+/* Firing Express App */
+const app = express();
+app.use(express.json()); // it allows access the data sent from frontend using req.body.
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, `client`))); // saying that our file html etc., will be served.
